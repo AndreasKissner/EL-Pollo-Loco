0d7f43feaf46ae32f50a39154b1b8de13a5ab885
Andreas Kissner <andikiss500@gmail.com>
2025-11-11 16:43:27 +0100

    Added chicken.class and place all MovableObject.class

--- js/game.js
+++ js/game.js
@@ -1,12 +1,14 @@
 let canvas
 let ctx;
-let character = new MovableObject();
+let character = new Character();
+let chicken = new Chicken();
 
 function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
 
    console.log("My character is", character);
+   console.log("My chicken is", chicken);
 
     //Notlösung
     /*     setTimeout(() => {
