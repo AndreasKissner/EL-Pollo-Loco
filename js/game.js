bedb3167fdf52f5b3e06d7975fb9711774308fae
Andreas Kissner <andikiss500@gmail.com>
2025-11-11 19:03:27 +0100

    Add in character.class and chicken.class img . And in movable_object loadImage Methode for the 2 classes

--- js/game.js
+++ js/game.js
@@ -1,14 +1,14 @@
 let canvas
 let ctx;
-let character = new Character();
-let chicken = new Chicken();
+
+let world = new World();
 
 function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
 
-   console.log("My character is", character);
-   console.log("My chicken is", chicken);
+    console.log("My character is", world.character);
+    console.log("My chicken is", world.enemies);
 
     //Notlösung
     /*     setTimeout(() => {
--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -3,6 +3,13 @@ class MovableObject {
    y = 400;
    img;
 
+
+
+   loadImage(path){
+      this.img = new Image('img/2_character_pepe/2_walk/W-21.png');
+      this.img.src = path;
+   }
+
    moveRight() {
       console.log("Moving right");
    }
