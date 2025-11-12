06a8aacd6539d1ae3b424c101a518d429ecd4b87
Andreas Kissner <andikiss500@gmail.com>
2025-11-12 07:47:46 +0100

    Added animaten character an d chicken

--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -5,6 +5,8 @@ class MovableObject {
    height = 150;
    width = 120;
    imageCache = {};
+    currentImage = 0;
+  
 
 
    loadImage(path) {
@@ -21,7 +23,7 @@ class MovableObject {
       arr.forEach(path => {
          let img = new Image();
          img.src = path;
-         this.imageCache[path] = path;
+         this.imageCache[path] = img;
       });
 
    }
