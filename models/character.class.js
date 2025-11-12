ff24dd370db3bc9a6008ff696a91e6b04693cfba
Andreas Kissner <andikiss500@gmail.com>
2025-11-12 18:48:17 +0100

    Added animation for walking character right and left

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
     height = 200;
     width = 150;
     y = 230;
+    speed= 10;
 
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
@@ -22,14 +23,25 @@ class Character extends MovableObject {
     }
 
     animate() {
+
+setInterval(() => {
+        if(this.world.keyboard.RIGHT){
+            this.x += this.speed;
+        }
+          if(this.world.keyboard.LEFT){
+            this.x -= this.speed;
+        }
+}, 1000/60);
+
         setInterval(() => {
-            if(this.world.keyboard.RIGHT){
+            if(this.world.keyboard.RIGHT ||this.world.keyboard.LEFT){
+                //Walk Animation
             let i = this.currentImage % this.IMAGES_WALKING.length;// let i = 0 % 6
             let path = this.IMAGES_WALKING[i];
             this.img = this.imageCache[path];
             this.currentImage++;
         }
-        }, 200);
+        }, 100);
     }
 
     jump() {
