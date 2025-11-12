8ffc720c53dc301f53f9288a5f0cb56d9f76c1a4
Andreas Kissner <andikiss500@gmail.com>
2025-11-12 06:22:26 +0100

    Added Logik for clouds moving

--- models/character.class.js
+++ models/character.class.js
@@ -2,7 +2,7 @@ class Character extends MovableObject {
 
     height = 200;
     width = 150;
-
+    y = 230;
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png')
     }
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,9 +7,14 @@ class Cloud extends MovableObject{
       constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png')
         this.x = Math.random() * 400
+        this.animate();
     }
 
-
+    animate(){
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000/60);
+    }
 
     
 }
