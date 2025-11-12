a7d1257788f83ebc175b3d70406028393bd0e241
Andreas Kissner <andikiss500@gmail.com>
2025-11-12 06:43:46 +0100

    Added all Charcter Walking images

--- models/background_object.class.js
+++ models/background_object.class.js
@@ -1,13 +1,11 @@
 class BackgroundObject extends MovableObject {
 
+    width = 720;
+    height = 480;
 
-
-width = 720;
-height = 480;
-
-    constructor(imagePath, x, y) {
+    constructor(imagePath, x,) {
         super().loadImage(imagePath);
-          this.y = 480 - this.height; // 480 - 400
+        this.y = 480 - this.height; // 480 - 400
         this.x = x;
     }
 
--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -2,22 +2,37 @@ class MovableObject {
    x = 40;
    y = 250;
    img;
-   height= 150;
+   height = 150;
    width = 120;
+   imageCache = {};
 
 
-   loadImage(path){
+   loadImage(path) {
       this.img = new Image('img/2_character_pepe/2_walk/W-21.png');
       this.img.src = path;
    }
-   
+
+
+   /**
+* 
+* @param {Arry} arr -['img/image1.png', 'img/image2.png' usw.]
+*/
+   loadImages(arr) {
+      arr.forEach(path => {
+         let img = new Image();
+         img.src = path;
+         this.imageCache[path] = path;
+      });
+
+   }
+
 
    moveRight() {
       console.log("Moving right");
    }
 
-    moveLeft(){
+   moveLeft() {
 
-    }
+   }
 
 }
