c47abf40f1dc291572cc7ddff43e83dbf12b5fb4
Andreas Kissner <andikiss500@gmail.com>
2025-11-24 17:08:05 +0100

    Refactor cloud.class.js added Js docs too

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,15 +1,18 @@
-class Cloud extends MovableObject{
-        y = 20;
-        width = 500;
-        height = 250;
+/** Represents a moving background cloud. */
+class Cloud extends MovableObject {
+  y = 20;
+  width = 500;
+  height = 250;
 
-      constructor() {
-        super().loadImage('img/5_background/layers/4_clouds/1.png')
-        this.x = Math.random() * 400
-        this.animate();
-    }
-
-    animate(){
-     this.moveLeft();
-    }
+  /** Initializes the cloud, loads its image, and sets a random start position. */
+  constructor() {
+    super().loadImage('img/5_background/layers/4_clouds/1.png')
+    this.x = Math.random() * 400
+    this.animate();
+  }
+  
+  /** Starts the cloud's movement animation. */
+  animate() {
+    this.moveLeft();
+  }
 }
