671e2555a0ea367c07beaf358d55e52b3660046b
Andreas Kissner <andikiss500@gmail.com>
2025-11-26 07:01:12 +0100

    Refactor Info Box and reaponsiv Style

--- js/button.js
+++ js/button.js
@@ -13,7 +13,19 @@ function toggleSound() {
 /**
  * Toggles the visibility of the keyboard instruction box.
  */
-function toggleKeyboardInfo() {
-    const box = document.getElementById('keyboardInstructions')
-    box.classList.toggle('hidden')
+function toggleKeyboardOverlay() {
+    const overlay = document.getElementById("screenOverlay");
+    const box = document.getElementById("keyboardInstructions");
+    overlay.classList.toggle("hidden");
+    box.classList.toggle("hidden");
+    if (!box.classList.contains("hidden")) {
+        document.body.appendChild(box);
+    }
+    overlay.onclick = () => {
+        overlay.classList.add("hidden");
+        box.classList.add("hidden");
+    };
 }
+
+
+
